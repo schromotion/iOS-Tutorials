36fdab1b914270f63d201b6fefa340d9800928ba
k.c. schroeter <kesc0188@colorado.edu>
2017-07-27 14:55:25 -0400

    Init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+var a = 5
+var b = 5
+
+print(a+b)
+
+if a+b > 8 {
+    print("true")
+}
+else {
+    print("false")
+}
